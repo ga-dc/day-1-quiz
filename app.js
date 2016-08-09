95cb889a5416ac8f4dbc7fa5cdf72bfce92134ac
Nick Olds <nolds9@yahoo.com>
2016-08-09 17:10:43 -0400

    adds tests for first 4 questions

--- app.js
+++ app.js
@@ -2,18 +2,18 @@ var questions = []
 
 Function.prototype.guts = function(){
   return this.toString()
-    .replace(/function\s\(\){/,'')
+    .replace(/function\s\(\)\s?{/,'')
     .replace(/}$/,'')
+    .replace(/`/g, "")
     .replace(/^\s+/g,'')
 }
 
 function describe(description, data){
   questions.push({
     description,
-    init: data.init,
+    init: data.init || function () { this.assertions = [] },
     starterCode: data.starterCode.guts(),
     tests: data.tests,
-    solution: data.solution.guts(),
     index: questions.length
   })
 }
@@ -32,16 +32,19 @@ function render(question){
   var source = document.getElementById("question").innerHTML
   var template = Handlebars.compile(source)
   document.querySelector(".questions").innerHTML += template(question)
-  var checkr = document.querySelector("[data-tests='"+question.index+"']")
-  checkr.addEventListener("click", function(event){
+  document.q
+}
+
+function onCheckClick (question) {
     var solution = document.querySelector("[data-solution='"+question.index+"']").value
-    question.init.call(question)
-    eval(solution)
+    if (typeof question.init == "function")question.init.call(question)
+    try {
+      eval(solution)
+    }
+    catch(e) {}
     question.tests.call(question)
-    console.dir(question.assertions)
     var results = document.querySelector("[data-results='"+question.index+"']")
     var source = document.getElementById("assertions").innerHTML
     var template = Handlebars.compile(source)
     results.innerHTML = template({assertions: question.assertions})
-  })
 }
--- quiz.js
+++ quiz.js
@@ -25,6 +25,72 @@ var list = [1,9,2,8,3,7,4,6,5]
   }
 })
 
-describe("fix the following html", function(){
+describe("fix the following html", {
+  starterCode: function() {
+    `<ul>
+    <li><a href=''>About Me</li></a>
+</ul>
+    `
+  },
+  tests: function () {
+    let solution = document.querySelector("[data-solution='"+this.index+"']").value
+    let matches = solution.match(/li(.*)a(.*)a(.*)li(.*)/) || []
 
+    expect(this, "Expects a to be nested inside li", true, matches.length > 0)
+
+  }
+})
+describe("define a function named modulo that takes two arguments, it should return the remainder of divding the first argument by the second", {
+  starterCode: function () {
+  },
+  tests: function () {
+    let solution = document.querySelector("[data-solution='"+this.index+"']").value
+    var modulo
+    eval(solution)
+    try {
+      expect(this, "the function modulo to be defined", 'function', typeof modulo)
+      expect(this, "when we divide 4 by 3, should return remainder 1 ", 1, modulo(3,4))
+      expect(this, "when we divide 7 by 5, should return remainder 2 ", 2, modulo(5,7))
+    } catch (e) {}   
+  }
+})
+describe("create an object", {
+  starterCode: function () {
+    // create a variable named pet, with the following properties and values:
+// species - iguana
+// gender - female
+// age - 12
+// name - godzilla
+var pet = {
+}
+  },
+  tests: function () {
+    let solution = document.querySelector("[data-solution='"+this.index+"']").value
+    eval(solution)
+    expect(this, "the variable pet should be an object", true, pet instanceof Object)
+    expect(this, "the pet's species should be iguana", "iguana", pet.species)
+    expect(this, "the pet's gender should be female", "female", pet.gender)
+    expect(this, "the pet's age should be 12", 12, pet.age)
+    expect(this, "the pet's name should be godzilla", "godzilla", pet.name)
+
+  }
+})
+describe("write CSS for the following HTML", {
+  starterCode: function () {
+    `<div class='red-square'></div>
+<!-- should be a red squre -->
+<div id='blue-circle'></div>
+<!-- should be a blue circle -->
+<style>
+// write CSS code here
+</style>
+`
+  },
+  tests: function () {
+    let solution = document.querySelector("[data-solution='"+this.index+"']").value
+    let numIds = solution.match(/#blue-circle/) || []
+    let numClasses = solution.match(/#blue-circle/) || []
+    expect(this, "number of id selectors to be 1", 1, numIds.length)
+    expect(this, "number of class selectors to be 1", 1, numIds.length)
+  }
 })
